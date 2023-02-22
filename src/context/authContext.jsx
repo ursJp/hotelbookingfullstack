a510ad607b1ce59d704bd2b5e6db8f66b3ab887e
jayaprakash naik <jayaprakashnaik.mude@gmail.com>
2023-02-22 15:24:45 +0530

    context

---
+++ src/context/authContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = () => {
+    setCurrentUser({
+      id: 1,
+      name: "john",
+      profilePic:
+        "https://cdn.pixabay.com/photo/2017/12/31/15/56/portrait-3052641__480.jpg",
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
